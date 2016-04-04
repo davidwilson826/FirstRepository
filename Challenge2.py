7dc6f7cff433de094ba3ab435edf64d4d8f0fba0
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-04 15:16:15 -0400

    Updated from Brython Server: 4/4/2016 3:16:14 PM

--- Challenge2.py
+++ Challenge2.py
@@ -3,4 +3,9 @@ parentheses = input("Enter string?")
 result = "true"
 
 for x in parentheses:
-    
+    if x == '(' or '{' or '[':
+        cut = parenteses
+        
+'''
+test everything in cut to see if it is valid
+remove cut from parentheses
