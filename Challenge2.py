50459b212b055772a9c3095e213e85eb381b6402
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-04 19:36:04 -0400

    Updated from Brython Server: 4/4/2016 7:36:02 PM

--- Challenge2.py
+++ Challenge2.py
@@ -7,7 +7,8 @@ result = "true"
 if parentheses[0] == '(' or '{' or '[':
     close = reference[reference.index(parentheses[0])+1]
     print(close)
-#    cut = parentheses[:parentheses.index(reference[reference.index
+    cut = parentheses[:parentheses.index(close)+1]
+    print(cut)
 else:
     result = "false"
         
