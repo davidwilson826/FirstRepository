e2b48d43b584b28e1c8d56c7af3b9d642a1f4e22
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-04 15:05:38 -0400

    Updated from Brython Server: 4/4/2016 3:05:38 PM

--- Challenge1.py
+++ Challenge1.py
@@ -1,1 +1,11 @@
+alpha = "abcdefghijklmnopqrstuvwxyz"
 
+strings = input("Enter two words?" ).split()
+
+result = "true"
+
+for x in alpha:
+    if strings[0].count(x) != strings[1].count(x):
+        result = "false"
+        
+print(result)
