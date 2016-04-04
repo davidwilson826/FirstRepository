1cfaf2301535af6f541a0dfb577fed40ca0f3ca3
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-04 17:14:31 -0400

    Updated from Brython Server: 4/4/2016 5:14:30 PM

--- Challenge2.py
+++ Challenge2.py
@@ -1,11 +1,17 @@
-parentheses = input("Enter string?")
+reference = ['(',')','{','}','[',']']
+
+parentheses = '[({})]'#input("Enter string?")
 
 result = "true"
 
-for x in parentheses:
-    if x == '(' or '{' or '[':
-        cut = parenteses
+if parentheses[0] == '(' or '{' or '[':
+    close = reference[reference.index(parentheses[0])+1]
+    print(close)
+#    cut = parentheses[:parentheses.index(reference[reference.index
+else:
+    result = "false"
         
 '''
 test everything in cut to see if it is valid
 remove cut from parentheses
+'''
