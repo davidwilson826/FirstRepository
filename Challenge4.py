b24dd3f18739782d7d8940343bc1a69ef01eea9a
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-04-04 20:58:36 -0400

    Updated from Brython Server: 4/4/2016 8:58:35 PM

--- Challenge4.py
+++ Challenge4.py
@@ -1,1 +1,24 @@
+rectangle1 = '-2 1 4 3'#input("Enter rectangle1: ")
+rectangle2 = '-5 -1 5 2'#input("Enter rectangle2: ")
+
+rectangle1 = [int(x) for x in rectangle1.split()]
+rectangle2 = [int(x) for x in rectangle2.split()]
+
+print(rectangle1, rectangle2)
+
+def points(rectangle):
+    rectpoints = [(rectangle[0], rectangle[1]) for x in list(range(0,4))]
+    rectpoints[1][0] += rectangle[2]
+    rectpoints[2][1] += rectangle[3]
+    rectpoints[3][0] += rectangle[2]
+    rectpoints[3][1] += rectangle[3]
+    
+points(rectangle1)
+rect1points = rectpoints
+points(rectangle2)
+rect2points = rectpoints
+
+print(rect1points, rect2points)
+
+result = "No collision"
 
