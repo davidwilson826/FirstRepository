8fb5c08024f3ee8d1f002799f627060a35a709d6
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-21 23:19:54 -0400

    Updated from Brython Server: 5/21/2016 11:19:53 PM

--- Challenge2Take2.py
+++ Challenge2Take2.py
@@ -1,1 +1,10 @@
+parens = '()[]{}'
 
+string = input('Enter string? ')
+
+success = True
+
+while len(string) > 0:
+    index = 0
+    while index < len(string):
+        if parens.index(string[index])+1 == parens.index(string[index+1]):
