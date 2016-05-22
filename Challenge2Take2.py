5ea84fece39db0972d4de4877924f1ef66ff9852
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-21 23:31:40 -0400

    Updated from Brython Server: 5/21/2016 11:31:39 PM

--- Challenge2Take2.py
+++ Challenge2Take2.py
@@ -7,11 +7,12 @@ success = True
 
 index = 0
 while len(string) > 0 and success == True:
-    while index < len(string):
+    while index < len(string)-1:
         if parens.index(string[index])+1 == parens.index(string[index+1]):
             string = string[:index]+string[index+2:]
         index += 1
+    index = 0
     if len(string) == length:
         success = False
     
-print(string)
+print(success)
