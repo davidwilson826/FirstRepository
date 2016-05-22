74367630e91ad2b5839d572e56094c409fdfd48e
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-22 11:58:31 -0400

    Updated from Brython Server: 5/22/2016 11:58:30 AM

--- C2Again.py
+++ C2Again.py
@@ -1,1 +1,14 @@
+string = input('Enter string? ')
 
+success = True
+
+while len(string) > 0 and success == True:
+    length = len(string)
+    for x in ['()','[]','{}']:
+        if x in string:
+            loc = string.index(x)
+            string = string[:loc].append(string[loc+2])
+    if length == len(string):
+        success = False
+        
+print(success)
