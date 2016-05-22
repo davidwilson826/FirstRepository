e2e0a84ad4fabcdba68117446634255bbb55297d
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-05-21 23:25:36 -0400

    Updated from Brython Server: 5/21/2016 11:25:34 PM

--- Challenge2Take2.py
+++ Challenge2Take2.py
@@ -1,13 +1,17 @@
 parens = '()[]{}'
 
 string = input('Enter string? ')
+length = len(string)
 
 success = True
 
 index = 0
-while index < len(string):
-    if parens.index(string[index])+1 == parens.index(string[index+1]):
-        string = string[:index]+string[index+1:]
-    index += 1
+while len(string) > 0 and success == True:
+    while index < len(string):
+        if parens.index(string[index])+1 == parens.index(string[index+1]):
+            string = string[:index]+string[index+2:]
+        index += 1
+    if len(string) == length:
+        success = False
     
 print(string)
