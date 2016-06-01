fb1614387970d63474cfd125ed24805aa3b62a58
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-06-01 14:12:10 -0400

    Updated from Brython Server: 6/1/2016 2:12:09 PM

--- Thing.py
+++ Thing.py
@@ -1,5 +1,17 @@
 from random import randint
 
 number = [str(randint(0,9)) for x in range(5)]
-
 print(number)
+
+guess = input("Guess a five digit number. ")
+print(guess)
+
+correct = 0
+
+for x in guess:
+    if guess.count(x) < number.count(x):
+        correct += guess.count(x)
+    else:
+        correct += number.count(x)
+        
+print(correct)
