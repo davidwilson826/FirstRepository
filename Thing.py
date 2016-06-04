8f05beee5f3bb875541deb6e824348d5cf230c77
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-06-04 17:47:10 -0400

    Updated from Brython Server: 6/4/2016 5:47:10 PM

--- Thing.py
+++ Thing.py
@@ -8,66 +8,71 @@ guesses = []
 
 def compare():
     
-    guess = input("Guess a five digit number. ")#'58995'
+    def makeGuess():
+        guess = input("Guess a five digit number. ")#'58995'
+        for x in guess:
+            if x not in '0123456789':
+                print('Not a five digit number. Try again.')
+                makeGuess()
+                
+    makeGuess()
     
     global attempts
     attempts += 1
+        
+    nfreq = [number.count(str(x)) for x in range(10)]
+    gfreq = [guess.count(str(x)) for x in range(10)]
+        
+    correct = 0
+    index = 0
+        
+    while index <= 9:
+        if nfreq[index] < gfreq[index]:
+            correct += nfreq[index]
+        else:
+            correct += gfreq[index]
+        index += 1
+            
+    guesses.append([attempts, guess, correct])
+                
+    print(str(correct)+' digits correct')
+        
+    correct = 0
+    index = 0
+        
+    while index <= 4:
+        if number[index] == guess[index]:
+            correct += 1
+        index += 1
+        
+    print(str(correct)+' digits correctly placed')
+        
+    guesses[-1].append(correct)
+        
+    print('\n'*50)
+    header = ['Attempt', 'Guess', 'Correct Digits', 'Correctly Placed']
+    eline = '='*52
+
+    print(eline)
+    for x in header:
+        print('||'+x,end='')
+    print('||')
+
+    for x in [list(zip(y, header)) for y in guesses]:
+        for a in x:
+            z = (str(a[0]), a[1])
+            spaces = ' '*int((len(z[1])-len(z[0]))/2)
+            print('||'+spaces+z[0]+spaces,end='')
+            if (len(z[1])-len(z[0]))%2 == 1:
+                print(' ',end='')
+        print('||')
+    print(eline+'\n')
     
     if ''.join(number) == guess:
-        print('\n====================\nCorrect! '+str(attempts)+' attempts.')
+        print('====================\nCorrect! '+str(attempts)+' attempts.')
         print(''.join(number))
         print('====================')
-        
     else:
-    
-        nfreq = [number.count(str(x)) for x in range(10)]
-        gfreq = [guess.count(str(x)) for x in range(10)]
-        
-        correct = 0
-        index = 0
-        
-        while index <= 9:
-            if nfreq[index] < gfreq[index]:
-                correct += nfreq[index]
-            else:
-                correct += gfreq[index]
-            index += 1
-            
-        guesses.append([attempts, guess, correct])
-                
-        print(str(correct)+' digits correct')
-        
-        correct = 0
-        index = 0
-        
-        while index <= 4:
-            if number[index] == guess[index]:
-                correct += 1
-            index += 1
-            
-        print(str(correct)+' digits correctly placed')
-        
-        guesses[-1].append(correct)
-        
-        print('\n'*50)
-        header = ['Attempt', 'Guess', 'Correct Digits', 'Correctly Placed']
-        eline = '='*52
-
-        print(eline)
-        for x in header:
-            print('||'+x,end='')
-        print('||')
-
-        for x in [list(zip(y, header)) for y in guesses]:
-            for a in x:
-                z = (str(a[0]), a[1])
-                spaces = ' '*int((len(z[1])-len(z[0]))/2)
-                print('||'+spaces+z[0]+spaces,end='')
-                if (len(z[1])-len(z[0]))%2 == 1:
-                    print(' ',end='')
-            print('||')
-        print(eline+'\n')
-        
         compare()
         
 compare()
