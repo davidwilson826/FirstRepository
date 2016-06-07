8c4770c6928beb7f175846e4d5396099bc2801ba
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-06-06 22:01:58 -0400

    Updated from Brython Server: 6/6/2016 10:01:56 PM

--- Challenge5.py
+++ Challenge5.py
@@ -3,6 +3,17 @@ n = 24
 
 factors = []
 
-for x in range(2,int(n/2)):
+for x in range(2,int(n/2)+1):
     if n%x == 0:
         factors.append(x)
+        
+print(factors)
+
+'''
+unique = []
+
+for x in possibilities:
+    if x not in unique:
+        unique.append(x)
+        
+print(len(unique))
